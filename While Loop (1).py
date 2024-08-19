dbe7a04b87d0a36494187ef983e946feb5c4e62d
Lakshaya Singh <97726095+lakshaya08@users.noreply.github.com>
2024-08-19 08:29:44 -0700

    Add files via upload

---
+++ While Loop (1).py
@@ -0,0 +1,6 @@
+message = str(input("Enter The Message You Want To Print: "))
+number_of_times = int(input("Enter Number Of Times You Want To Print Message: "))
+variable = 0
+while variable<number_of_times:
+    print(message)
+    variable = variable + 1
